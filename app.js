0d94e8b499304cfd8deac2e9a281e45e4455b58f
agusnarvaez <agus.narvaez@outlook.com>
2021-09-06 20:43:41 -0300

    Primer commit

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const path = require('path');
 const publicPath = path.resolve(__dirname, './public');
 const dir = 3050;
 app.use(express.static(publicPath));
-app.listen(dir, () => {
+/*app.listen(dir, () => {
     console.log('Servidor corriendo');
     console.log('http://localhost:'+dir+'/');
-});
+});*/
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log('Servidor corriendo en el puerto 3000');
+})
 
 app.get('/', (req, res) => {
     //let htmlPath = path.resolve(__dirname, './views/index.html');
